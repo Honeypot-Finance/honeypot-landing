7ff8518162732872acfd0f6e455c2f11486a9dd1
sebastian3500 <sebastianrosa39@gmail.com>
2023-07-30 23:38:00 -0400

    commit

--- pages/index.vue
+++ pages/index.vue
@@ -8,11 +8,11 @@
         <img src="~/assets/sources/images/berachain.png" alt="Berachain"> Built on Berachain
       </v-btn>  
       <h2>
-        A Continuous <br> Innovative De-fi hub
+        A Continuous <br> Innovative De-fi Hub
       </h2>
 
       <div class="center divrow mobile-div" style="gap: 10px; background-color: transparent;">
-        <v-btn  v-for="(item, index) in dataBtns" :key="index" class="btn-data">
+        <v-btn  v-for="(item, index) in dataBtns" :key="index" class="btn-data" :href="item.link" target="_blank">
           <img class="btn-img" :src="item.img" alt="Image"> <img class="btn-img-black" :src="item.img_black" alt="Image"> {{ item.text }}
         </v-btn>
       </div>
@@ -31,21 +31,25 @@ export default {
           text: "Twitter",
           img:  require("~/assets/sources/icons/twitter.svg"),
           img_black: require("~/assets/sources/icons/twitter-black.svg"),
+          link:"https://twitter.com/honeypotfinance",
         },
         {
           text: "Frontend Work",
           img:  require("~/assets/sources/icons/frontend.svg"),
           img_black: require("~/assets/sources/icons/frontend-black.svg"),
+          link:"https://honeypot-front-end-ten.vercel.app/swap",
         },
         {
           text: "Discord",
           img:  require("~/assets/sources/icons/discord.svg"),
           img_black: require("~/assets/sources/icons/discord-black.svg"),
+          link:"https://discord.com/invite/pkh7shAPWB",
         },
         {
           text: "Prototype",
           img:  require("~/assets/sources/icons/arrow.svg"),
           img_black: require("~/assets/sources/icons/arrow-black.svg"),
+          link:"https://yexlab.vercel.app/main",
         },
       ]
     }
