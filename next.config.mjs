26df3bae3dafe0bdddf7e6f03c0abc5d55e02648
Hongming Wang <hongmingwangrabbit@gmail.com>
2025-08-13 10:18:47 -0700

    try to rewrite path

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
   sassOptions: {
     includePaths: ["./src/styles"],
   },
+  async rewrites() {
+    return [
+      { source: "/swap", destination: "https://wasabee.honeypotfinance.xyz/" },
+      {
+        source: "/swap/:path*",
+        destination: "https://wasabee.honeypotfinance.xyz/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
