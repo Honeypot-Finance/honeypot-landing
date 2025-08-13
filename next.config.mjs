ae2e9bd5b308c235551d7500fe87f79dad0fd9b1
Hongming Wang <hongmingwangrabbit@gmail.com>
2025-08-13 10:53:27 -0700

    update redirects

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,21 @@ const nextConfig = {
   sassOptions: {
     includePaths: ["./src/styles"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/swap",
+        destination: "https://wasabee.honeypotfinance.xyz/swap",
+        permanent: false,
+      },
+
+      {
+        source: "/cross-chain-swap",
+        destination: "https://wasabee.honeypotfinance.xyz/cross-chain-swap",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
